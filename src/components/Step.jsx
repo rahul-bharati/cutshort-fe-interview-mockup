946313099b0d4fe9d0575de694bd67e4e91b0674
Rahul Bharati <rahulrbharati@gmail.com>
2022-08-11 14:45:04 +0530

    animate step line

--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -1,13 +1,40 @@
+import { useContext } from "react";
+import { OnboardingContext } from "./../context/OnboardingContext";
+
 const Step = () => {
+  const { currentStep } = useContext(OnboardingContext);
+
+  console.log({ currentStep });
+
   return (
     <div className="step-container">
-      <div className="step-counter">1</div>
-      <div className="divider"></div>
-      <div className="step-counter">2</div>
-      <div className="divider"></div>
-      <div className="step-counter">3</div>
-      <div className="divider"></div>
-      <div className="step-counter">4</div>
+      <div className={`step-counter ${currentStep >= 0 ? "fill" : ""}`}>1</div>
+      <div
+        className={`divider ${
+          currentStep >= 0 && currentStep < 1 ? "half-fill" : "fill"
+        }`}
+      ></div>
+      <div className={`step-counter ${currentStep >= 1 ? "fill" : ""}`}>2</div>
+      <div
+        className={`divider ${
+          currentStep >= 1 && currentStep < 2
+            ? "half-fill"
+            : currentStep < 1
+            ? ""
+            : "fill"
+        }`}
+      ></div>
+      <div className={`step-counter ${currentStep >= 2 ? "fill" : ""}`}>3</div>
+      <div
+        className={`divider ${
+          currentStep >= 2 && currentStep < 3
+            ? "half-fill"
+            : currentStep < 3
+            ? ""
+            : "fill"
+        }`}
+      ></div>
+      <div className={`step-counter ${currentStep >= 3 ? "fill" : ""}`}>4</div>
     </div>
   );
 };
